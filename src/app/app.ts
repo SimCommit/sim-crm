8c6a4eab7e0874f564359bca7267a86fb34b33ec
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-12 22:18:31 +0100

    feat: Add dashboard and user components with routing and basic templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatSidenavModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
