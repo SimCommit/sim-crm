98d9e19842c2afefa18bd8744f13492767873ded
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-12 21:56:43 +0100

    feat: Implement sidebar navigation and update styles
    - Added a sidebar navigation using Angular Material's MatDrawer component in app.html.
    - Created a menu with icons for Dashboard and User in the sidebar.
    - Updated app.scss to style the sidebar and menu items.
    - Modified app.ts to include MatToolbarModule for the toolbar.
    - Updated styles in styles.scss to change the primary and tertiary color palettes for Angular Material theming.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatSidenavModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
