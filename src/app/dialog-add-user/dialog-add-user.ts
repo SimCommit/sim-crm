b6a3685736311238dda54ceb4e1c2a56881e8837
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-14 18:27:12 +0100

    feat: Create dialog for adding users with appropriate layout and actions

--- src/app/dialog-add-user/dialog-add-user.ts
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDialogContent, MatDialogActions } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [],
+  imports: [MatDialogContent, MatDialogActions, MatDialogContent],
   templateUrl: './dialog-add-user.html',
   styleUrl: './dialog-add-user.scss',
 })
