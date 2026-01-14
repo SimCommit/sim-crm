0c043e12c008a0d069ccd503827ea016e2571108
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-14 18:11:34 +0100

    feat: Implement dialog for adding users and update user component with dialog integration

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
+  public dialog = inject(MatDialog)
 
+  openDialog() {
+    this.dialog.open(DialogAddUser)
+  }
 }
