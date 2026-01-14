f9a5957c6356089a3e0413680d39d6d9c8800b43
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-14 19:56:01 +0100

    feat: Enhance user dialog with form fields for user details and date selection

--- src/app/dialog-add-user/dialog-add-user.ts
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { MatDialogContent, MatDialogActions } from "@angular/material/dialog";
+import { MatButton } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogContent, MatDialogActions, MatDialogContent],
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogContent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButton,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   templateUrl: './dialog-add-user.html',
   styleUrl: './dialog-add-user.scss',
 })
-export class DialogAddUser {
-
-}
+export class DialogAddUser {}
