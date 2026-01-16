3d0dfa9f63a4bca16d7917795ccd6ff2cb185490
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-17 00:19:30 +0100

    feat: add Firebase configuration and update app module for Firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideNativeDateAdapter()
-  ]
+    provideNativeDateAdapter(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'sim-crm-caa9a',
+        appId: '1:724678862277:web:a9857f0c53aa45a86e17ac',
+        storageBucket: 'sim-crm-caa9a.firebasestorage.app',
+        apiKey: 'AIzaSyBBw9iseaS4CcP6wS06VBW2Rvt3xKRYTxY',
+        authDomain: 'sim-crm-caa9a.firebaseapp.com',
+        messagingSenderId: '724678862277',
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
