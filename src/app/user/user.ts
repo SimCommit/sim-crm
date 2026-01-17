6d4b3963d2d572a7d266fb34a6d10368950d214a
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-17 16:56:56 +0100

    feat: update user component layout with a new table structure and enhance styling

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -4,18 +4,20 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
-  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule],
+  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule, MatCard, MatCardContent],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
   public dialog = inject(MatDialog)
  
-
   openDialog() {
     this.dialog.open(DialogAddUser)
   }
+
+  
 }
