1007469b03ece139d6b516336ddb80b53305c32f
SimCommit <168715756+SimCommit@users.noreply.github.com>
2026-01-17 21:07:54 +0100

    feat: add favicon and logo images, update logo display in the app layout

--- src/app/shared/services/user-data.service.ts
+++ src/app/shared/services/user-data.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, Injector, runInInjectionContext, signal } from '@angular/core';
-import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { User } from '../../../models/user.data';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,12 @@ import { User } from '../../../models/user.data';
 export class UserDataService {
   private firestore = inject(Firestore);
   private injector = inject(Injector);
+
+  // users$!: Observable<User[]>;
+
+  // connectUserDataStream(): void {
+  //   this.users$ = collectionData(this.getUserRef());
+  // }
 
   // await und async vor allen relevanten Funktionsteilen
   async addUser(user: User): Promise<void> {
